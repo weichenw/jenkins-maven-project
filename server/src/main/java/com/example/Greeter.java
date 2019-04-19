7729a3bc459d4b9d1764a86d303ac6a7b30f5818
Wei Chen <weichenw@gmail.com>
2019-04-19 23:13:32 +1000

    Create unused fields for spotbugs plugin test

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,4 +16,24 @@ public class Greeter {
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
+
+  //DEBUG: Test Spotbugs plugin
+      //Unused field
+    private int abc;
+
+    private String ip = "127.0.0.1";
+
+    public void test() {
+
+        String[] field = {"a", "b", "c", "s", "e"};
+
+        //concatenates strings using + in a loop
+        String s = "";
+        for (int i = 0; i < field.length; ++i) {
+            s = s + field[i];
+        }
+
+        System.out.println(ip);
+
+    }
 }
